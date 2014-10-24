aa1554a95ab1aacd1c01a2c38e673cfa4b6554fc
blackboyjakk <blackboyjakk@gmail.com>
2014-10-25 02:18:19 +0700

    fine d

--- assignmet 2/assignmet 2/Program.cs
+++ assignmet 2/assignmet 2/Program.cs
@@ -8,29 +8,19 @@ namespace assignmet_2
 {
     class Program
     {
-        static uint randomNum() { 
-            int max = (int)Math.Pow(2, 29);
-            Random random = new Random();
-            uint number = (uint)random.Next(0, max);
-            uint bignum = (uint)random.Next(0, 2);
-
-            Console.WriteLine((uint)Math.Pow(2, 30) +1);
-            Console.WriteLine((uint)Math.Pow(2,32)-1);
-          
-            uint resuilt = bignum*(uint)Math.Pow(2, 31) + (uint)Math.Pow(2, 30) + (uint)random.Next(max) * 2 + 1;
-            return resuilt;
+        static uint randomNum(uint min,uint max) { 
+                           
+            Random random = new Random(Environment.TickCount);
+            uint randonNumber = (uint)(min/2+(max-min)/2*random.NextDouble())*2+1;
+            return randonNumber;
         }
 
-       static uint getPrime(uint min) { 
-              // Console.WriteLine(rand());
-//            long min = (long)Math.Pow(2, 30)+1;
-            uint max = (uint)Math.Pow(2, 32)-1;
- //           Console.WriteLine(min);
-   //         Console.WriteLine(max);
-  //          int count = 0;
-            for (uint i = min; i <= max; i += 2) {
+       static uint getPrime(uint nonPrime,uint min,uint max) { 
+
+           
+            for (uint i = nonPrime; i < max; i += 2) {
                 bool primeFound = true;
-                int maxPrimeSeek = (int)Math.Sqrt(i) + 1;
+                uint maxPrimeSeek = (uint)Math.Sqrt(i) + 1;
                 for (uint j = 3; j <= maxPrimeSeek; j += 2)
                 {
                     if ((i % j)==0)
@@ -47,19 +37,39 @@ namespace assignmet_2
                 if (i == max) {
                         i = (uint)Math.Pow(2, 30) + 1;
                 }
-                   // Console.WriteLine(i);
-                    //count++;
             }
-            Console.WriteLine((uint)Math.Pow(2, 30) + 1);
-            return getPrime((uint)Math.Pow(2,30)+1);//Console.WriteLine("among = "+count);
+           
+            return 0;
         }
 
         static void Main(string[] args)
-        {
-                uint randnum = randomNum();
-                Console.WriteLine(randnum);
-                Console.WriteLine(getPrime(randnum));
-            
+       {
+           uint max = (uint)Math.Pow(2, 32)-1;
+           uint min = (uint)Math.Pow(2, 30)+1;
+             //random p  
+          randomNum(min, max);
+           uint randomP = randomNum(min,max);
+           Console.WriteLine(randomP);
+           uint primeP = getPrime(randomP,min,max);
+            //random q
+          randomNum(min, max);
+           uint randomQ = randomNum(min,max);
+           Console.WriteLine(randomQ);
+           uint primeQ = getPrime(randomQ,min,max);
+            //n = p x q
+           ulong n = (ulong)primeP *(ulong)primeQ;
+           Console.Write("n = "+primeP+" x "+primeQ);
+           Console.WriteLine(" = "+n);
+            //phi=(p-1)x(q-1)
+           ulong phi = (ulong)(primeP-1) * (ulong)(primeQ-1);
+           Console.Write("phi n = (" + primeP + "-1) x (" + primeQ+"-1)");
+           Console.WriteLine(" = " + phi);
+            //d=1/e mod 
+           int e = 65537; 
+           double e_1= Math.Pow(e, -1D);
+           double d = (ulong)(((e_1 * phi) + e_1) %phi);
+           Console.WriteLine(d);
+           Console.WriteLine(((phi+1)*e_1)%phi);
         }
     }
 }
